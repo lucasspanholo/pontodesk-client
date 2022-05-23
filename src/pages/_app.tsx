dfab9b1026f628293bfc8440321d472aa3eac9e9
Allan <allan.sartor@pontomais.com.br>
2022-05-22 23:16:37 -0300

    Remove server fake mirageJs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,11 @@ import { theme } from '../styles/theme'
 
 import { QueryClientProvider } from 'react-query'; 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
-import { makeServer } from '../services/mirage'
+// import { makeServer } from '../services/mirage'
 import { queryClient } from '../services/queryClient';
 
 if (process.env.NODE_ENV === 'development') {
-  makeServer();
+  // makeServer();
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
