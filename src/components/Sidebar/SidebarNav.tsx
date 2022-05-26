ce3707a4fa9c5badeeaf9b2e8f245b35d2fea49d
Allan-Sartor <allansartor@outlook.com>
2022-05-26 15:22:18 -0300

    Changes in menu sidebar

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -1,5 +1,5 @@
 import { Stack } from "@chakra-ui/react";
-import { RiContactsLine, RiFileChartLine, RiLineChartLine, RiMoneyDollarBoxLine, RiStore3Line } from "react-icons/ri";
+import { RiTeamFill, RiTaskLine, RiLineChartLine } from "react-icons/ri";
 import { NavLink } from "./NavLink";
 import { NavSection } from "./NavSection";
 
@@ -8,15 +8,8 @@ export function SidebarNav() {
     <Stack spacing="12" align="flex-start">
       <NavSection title="GERAL">
         <NavLink icon={RiLineChartLine} href="/dashboard">Dashboard</NavLink>
-        <NavLink icon={RiContactsLine} href="/users">Clientes</NavLink>
-      </NavSection>
-
-      <NavSection title="ESTOQUE">
-        <NavLink icon={RiStore3Line} href="/forms">Produtos</NavLink>
-      </NavSection>
-
-      <NavSection title="ENTRADAS E SAÍDAS">
-        <NavLink icon={RiMoneyDollarBoxLine} href="/forms">Caixa</NavLink>
+        <NavLink icon={RiTaskLine} href="/calls">Chamados</NavLink>
+        <NavLink icon={RiTeamFill} href="/users">Usuários</NavLink>
       </NavSection>
     </Stack>
   );
