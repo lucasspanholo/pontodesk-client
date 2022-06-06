187e6594fd9998e6ce532d42962bf473320ccaf5
Allan Sartor <allansartor@outlook.com>
2022-06-06 00:44:35 -0300

    Create new endpoint for API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,3 +1,8 @@
+import axios from 'axios';
 import { getAPIClient } from './axios';
 
-export const api = getAPIClient()
+export const api = getAPIClient();
+
+export const apiCreateUser = axios.create({
+  baseURL: 'http://localhost:3001/'
+})
