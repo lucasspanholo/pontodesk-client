a84314e8d2563e6d7002eec632e17c062b18d2a8
Allan-Sartor <allansartor@outlook.com>
2022-06-06 22:42:07 -0300

    Deleted imports

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,13 +17,10 @@ import {
   useBreakpointValue,
 } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
-import { Header } from "../../components/Header";
-import { Pagination } from "../../components/Pagination";
-import { Sidebar } from "../../components/Sidebar";
 
 import NextLink from "next/link";
 import { getUsers, useUsers } from "../../services/hooks/useUsers";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 import { queryClient } from "../../services/queryClient";
 import { api } from "../../services/api";
@@ -60,99 +57,99 @@ export default function UserList({ users, pagination }) {
   // }
 
   return (
-   <Card>
-    <Box flex="1" borderRadius={8} bg="gray.800" p="8">
-          <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg" fontWeight="normal">
-              Usuários
+    <Card>
+      <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+        <Flex mb="8" justify="space-between" align="center">
+          <Heading size="lg" fontWeight="normal">
+            Usuários
 
-              {/* { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/> } */}
-            </Heading>
+            {/* { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/> } */}
+          </Heading>
 
-            <NextLink href="/users/create">
-              <Button
-                as="a"
-                size="sm"
-                fontSize="sm"
-                colorScheme="pink"
-                cursor="pointer"
-                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-              >
-                Criar novo
-              </Button>
-            </NextLink>
+          <NextLink href="/calls/create">
+            <Button
+              as="a"
+              size="sm"
+              fontSize="sm"
+              colorScheme="pink"
+              cursor="pointer"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </NextLink>
+        </Flex>
+
+        {isLoading ? (
+          <Flex justify="center">
+            <Spinner />
           </Flex>
+        ) : error ? (
+          <Flex justify="center">
+            <Text>Falha ao obter todos os chamados!</Text>
+          </Flex>
+        ) : (
+          <>
+            <Table colorScheme="whiteAlpha">
+              <Thead>
+                <Tr>
+                  <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                    <Checkbox colorScheme="pink" />
+                  </Th>
+                  <Th>Usuário</Th>
+                  {isWideVersion && <Th>Data de cadastro</Th>}
+                  <Th width="8"></Th>
+                </Tr>
+              </Thead>
 
-           { isLoading ? (
-            <Flex justify="center">
-              <Spinner />
-            </Flex>
-          ) : error ? (
-            <Flex justify="center">
-              <Text>Falha ao obter os dados dos usuários</Text>
-            </Flex>
-          ) : (
-            <>
-              <Table colorScheme="whiteAlpha">
-                <Thead>
-                  <Tr>
-                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
-                      <Checkbox colorScheme="pink" />
-                    </Th>
-                    <Th>Usuário</Th>
-                    { isWideVersion && <Th>Data de cadastro</Th> }
-                    <Th width="8"></Th>
-                  </Tr>
-                </Thead>
+              <Tbody>
+                {users.map(user => {
+                  return (
+                    <Tr key={user.id}>
+                      <Td px={["4", "4", "6"]}>
+                        <Checkbox colorScheme="pink" />
+                      </Td>
+                      <Td>
+                        <Box>
+                          <Link color="purple.400">
+                            <Text fontWeight="bold">{user.name}</Text>
+                          </Link>
+                          <Text fontSize="sm" color="gray.300">
+                            {user.email}
+                          </Text>
+                        </Box>
+                      </Td>
+                      {isWideVersion && <Td>{user.created_at}</Td>}
+                      <Td>
+                        {isWideVersion && (
+                          <Button
+                            as="a"
+                            size="sm"
+                            cursor="pointer"
+                            fontSize="sm"
+                            colorScheme="purple"
+                            leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                          >
+                            Editar
+                          </Button>
+                        )}
+                      </Td>
+                    </Tr>
+                  )
+                })}
+              </Tbody>
+            </Table>
 
-                <Tbody>
-                  { users.map(user => {
-                    return (
-                      <Tr key={ user.id }>
-                        <Td px={["4", "4", "6"]}>
-                          <Checkbox colorScheme="pink" />
-                        </Td>
-                        <Td>
-                          <Box>
-                            <Link color="purple.400">
-                              <Text fontWeight="bold">{ user.name }</Text>
-                            </Link>
-                            <Text fontSize="sm" color="gray.300">
-                              { user.email }
-                            </Text>
-                          </Box>
-                        </Td>
-                        { isWideVersion && <Td>{ user.created_at }</Td>}
-                        <Td>
-                          { isWideVersion && (
-                            <Button
-                              as="a"
-                              size="sm"
-                              cursor="pointer"
-                              fontSize="sm"
-                              colorScheme="purple"
-                              leftIcon={<Icon as={ RiPencilLine } fontSize="16" />}
-                            >
-                              Editar
-                            </Button>
-                          )}
-                        </Td>
-                      </Tr>
-                    )
-                  })}
-                </Tbody>
-              </Table>
-
-              {/* <Pagination 
+            {/* <Pagination 
                 totalCountOfRegisters={pagination.meta.totalItems}
                 currentPage={pagination.meta.current_page}
                 registerPerPage={pagination.meta.itemsPerPage}
                 onPageChange={setPage}
               /> */}
-            </>
-          )}
-        </Box>
-   </Card>
+          </>
+        )}
+      </Box>
+    </Card>
   );
 }
 
